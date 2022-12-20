f7ecdfb21b8cbb48e804cf2cd53ad84cf851b778
K-Maureen <kariukimaureen841@gmail.com>
2022-12-19 16:31:43 -0800

    edit file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints a given phrase
- * return: 0
+ * return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
