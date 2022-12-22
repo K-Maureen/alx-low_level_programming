a15c2348a9280e445b3266755d749d8ccdeddfcb
K-Maureen <kariukimaureen841@gmail.com>
2022-12-22 12:54:28 -0800

    edit file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strcmp - Function which compare value of two strings
+ * _strcmp - compares two string values
  * @s1: string 1
  * @s2: string 2
  * Return: the difference in s1 and s2
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2);
 {
 	int i;
 
